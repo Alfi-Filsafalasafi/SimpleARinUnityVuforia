d0a96af5f87571ed944f8217f902e50480928a35
Alfi-Filsafalasafi <filsafalasafi@gmail.com>
2022-03-10 17:45:36 +0700

    asset

---
+++ FocusCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using Vuforia;
+
+public class FocusCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       OnSingleTapped(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)) {
+            Application.Quit();
+        }
+        OnSingleTapped();
+    }
+
+    public void OnSingleTapped(){
+        TriggerAutoFocus();
+    }
+    public void TriggerAutoFocus(){
+        StartCoroutine(TriggerAutoFocusAndEnableContinuousFocusIfSet());
+    }
+    public IEnumerator TriggerAutoFocusAndEnableContinuousFocusIfSet(){
+        yield return new WaitForSeconds(0.5f);
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+}
+
